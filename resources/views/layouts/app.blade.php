9cd31bb4b156ec4968a6170808dfebfb8d50d7e1
Irsyad <irsyadizzuddin1999@gmail.com>
2023-02-08 15:08:31 +0800

    modified:   resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
 <nav class="navbar navbar-light bg-light">
     <div class="container">
         <a href="/"><span class="navbar-brand mb-0 h1">Todo</span></a>
-        <a href="/create"><span class="btn btn-primary">Create Todo</span></a>
+       
     </div>
 </nav>
 
